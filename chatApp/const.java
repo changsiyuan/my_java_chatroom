848c9a6329b9eb96eb597795b6ff4b394d7e83c0
changsiyuan <changsiyuan2011@126.com>
2014-11-16 11:03:04 +0800

    Create const.java

---
+++ chatApp/const.java
@@ -0,0 +1,12 @@
+/*
+author:changsiyuan
+*/
+package chatApp;
+
+/**
+ * Class
+ */
+public abstract class Const {
+    public static final int Port = 12345;
+    public static final String IP = "127.0.0.1";
+}
